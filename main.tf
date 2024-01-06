3a32612c5e76021ea896500414a78917f454dde0
Renee Osbourne <reneeosbourne@gmail.com>
2024-01-05 23:55:17 -0500

    Update main.tf file

--- main.tf
+++ main.tf
@@ -2,7 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "server1" {
   ami           =  "ami-079db87dc4c10ac91"
   instance_type = "t2.micro"
+
+  tags= {
+    Name = "tf-server1"
+  }
 }
